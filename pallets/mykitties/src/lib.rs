2acc5076b0d23b53e636086613851ef687d38aaf
Fazeel Usmani <fazeelusmani18@gmail.com>
2022-08-23 23:50:52 +0530

    Action Items

--- pallets/mykitties/src/lib.rs
+++ pallets/mykitties/src/lib.rs
@@ -4,16 +4,44 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{sp_runtime::traits::{Hash, Zero},
-                        dispatch::{DispatchResultWithPostInfo, DispatchResult}, 
-                        traits::{Currency, ExistenceRequirement, Randomness},
-                        pallet_prelude::*};
+  use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
-    use sp_core::H256;
+    use frame_support::{
+        sp_runtime::traits::Hash,
+        traits::{ Randomness, Currency, tokens::ExistenceRequirement },
+        transactional
+    };
+    use sp_io::hashing::blake2_128;
 
-    // TODO Part II: Struct for holding Kitty information.
+    #[cfg(feature = "std")]
+    use serde::{Deserialize, Serialize};
 
-    // TODO Part II: Enum and implementation to handle Gender type in Kitty struct.
+    // ACTION #1: Write a Struct to hold Kitty information.
+    type AccountOf<T> = <T as frame_system::Config>::AccountId;
+    type BalanceOf<T> = 
+        <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+    #[derive(Clone, Encode, Decode, PartialEq)]
+    pub struct Kitty<T: Config> {
+        pub dna: [u8; 16],
+        pub price: Option<BalanceOf<T>>,
+        pub gender: Gender,
+        pub owner: AccountOf<T>,
+    }
+    
+    // ACTION #2: Enum declaration for Gender.
+    #[derive(Encode, Decode, Debug, Clone, PartialEq)]
+    pub enum Gender {
+        Male, 
+        Female,
+    }
+
+    // ACTION #3: Implementation to handle Gender type in Kitty struct.
+    impl Default for Gender {
+        fn default() -> Self {
+            Gender::Male
+        }
+    }
+
 
     #[pallet::pallet]
     #[pallet::generate_store(trait Store)]
@@ -24,12 +52,12 @@ pub mod pallet {
     pub trait Config: pallet_balances::Config + frame_system::Config {
         /// Because this pallet emits events, it depends on the runtime's definition of an event.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-
+        
         /// The Currency handler for the Kitties pallet.
         type Currency: Currency<Self::AccountId>;
-        
-        // TODO Part II: Specify the custom types for our runtime.
 
+        // ACTION #5: Specify the type for Randomness we want to specify for runtime.
+        type KittyRandomness: Randomness<Self::H256, Self::BlockNumber>;
     }
 
     // Errors.
@@ -38,18 +66,25 @@ pub mod pallet {
         // TODO Part III
     }
 
+    // Events.
     #[pallet::event]
-    #[pallet::metadata(T::AccountId = "AccountId")]
+    // #[pallet::metadata(T::AccountId = "AccountId")]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         // TODO Part III
     }
 
-    // ACTION: Storage item to keep a count of all existing Kitties.
+    #[pallet::storage]
+    #[pallet::getter(fn all_kitties_count)]
+    pub(super) type AllKittiesCount<T: Config> = StorageValue<_, u64, ValueQuery>;
+    
+    // ACTION #6: Add Nonce storage item.
 
-    // TODO Part II: Remaining storage items.
+    // ACTION #9: Remaining storage items.
+    #[pallet::constant]
+    type MaxKittyOwned: Get<u32>;
 
-    // TODO Part III: Our pallet's genesis configuration.
+    // TODO Part IV: Our pallet's genesis configuration.
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
@@ -65,116 +100,48 @@ pub mod pallet {
         // TODO Part III: breed_kitty
     }
 
-    // TODO Parts II: helper function for Kitty struct
+    // ACTION #4: helper function for Kitty struct
+    fn gen_gender()->Gender {
+        let random = T::KittyRandomness::random(&b"gender"[..]).0;
+        match random.as_ref()[0] % 2 {
+            0 => Gender::Male,
+            _ => Gender::Female,
+        }
+    }
 
     impl<T: Config> Pallet<T> {
         // TODO Part III: helper functions for dispatchable functions
         
-        // TODO: increment_nonce, random_hash, mint, transfer_from
+        // ACTION #7: increment_nonce helper
+        fn gen_dna() -> [u8; 16] {
+            let payload = (
+                T::KittyRandomness::random(&b"dna"[..]).0,
+                <frame_system::Pallet<T>>::block_number(),
+            );
+            payload.using_encoded(blake2_128)
+        }
+
+        // ACTION #8: random_hash helper
+        #[pallet::storage]
+        #[pallet::getter(fn kitty)]
+        pub(super) type Kitties<T: Config> = StorageMap<
+            _, 
+            Twox64Concat, 
+            T::Hash, 
+            Kitty<T>
+            >;
+        #[pallet::storage]
+        #[pallet::getter(fn kitties_owned)]
+        pub(super) type Kitties<T: Config> = StorageMap<
+            _, 
+            Twox64Concat, 
+            T::AccountId, 
+            BoundedVec<T::Hash, T::MaxKittyOwned>,
+            ValueQuery
+            >;
+
+
+        // TODO: mint, transfer_from
         
     }
 }
-
-
-// #![cfg_attr(not(feature = "std"), no_std)]
-
-// /// Edit this file to define custom logic or remove it if it is not needed.
-// /// Learn more about FRAME and the core library of Substrate FRAME pallets:
-// /// <https://docs.substrate.io/v3/runtime/frame>
-// pub use pallet::*;
-
-// #[cfg(test)]
-// mod mock;
-
-// #[cfg(test)]
-// mod tests;
-
-// #[cfg(feature = "runtime-benchmarks")]
-// mod benchmarking;
-
-// #[frame_support::pallet]
-// pub mod pallet {
-// 	use frame_support::pallet_prelude::*;
-// 	use frame_system::pallet_prelude::*;
-
-// 	/// Configure the pallet by specifying the parameters and types on which it depends.
-// 	#[pallet::config]
-// 	pub trait Config: frame_system::Config {
-// 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
-// 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-// 	}
-
-// 	#[pallet::pallet]
-// 	#[pallet::generate_store(pub(super) trait Store)]
-// 	pub struct Pallet<T>(_);
-
-// 	// The pallet's runtime storage items.
-// 	// https://docs.substrate.io/v3/runtime/storage
-// 	#[pallet::storage]
-// 	#[pallet::getter(fn something)]
-// 	// Learn more about declaring storage items:
-// 	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-// 	pub type Something<T> = StorageValue<_, u32>;
-
-// 	// Pallets use events to inform users when important changes are made.
-// 	// https://docs.substrate.io/v3/runtime/events-and-errors
-// 	#[pallet::event]
-// 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-// 	pub enum Event<T: Config> {
-// 		/// Event documentation should end with an array that provides descriptive names for event
-// 		/// parameters. [something, who]
-// 		SomethingStored(u32, T::AccountId),
-// 	}
-
-// 	// Errors inform users that something went wrong.
-// 	#[pallet::error]
-// 	pub enum Error<T> {
-// 		/// Error names should be descriptive.
-// 		NoneValue,
-// 		/// Errors should have helpful documentation associated with them.
-// 		StorageOverflow,
-// 	}
-
-// 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-// 	// These functions materialize as "extrinsics", which are often compared to transactions.
-// 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
-// 	#[pallet::call]
-// 	impl<T: Config> Pallet<T> {
-// 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-// 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-// 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-// 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-// 			// Check that the extrinsic was signed and get the signer.
-// 			// This function will return an error if the extrinsic is not signed.
-// 			// https://docs.substrate.io/v3/runtime/origins
-// 			let who = ensure_signed(origin)?;
-
-// 			// Update storage.
-// 			<Something<T>>::put(something);
-
-// 			// Emit an event.
-// 			Self::deposit_event(Event::SomethingStored(something, who));
-// 			// Return a successful DispatchResultWithPostInfo
-// 			Ok(())
-// 		}
-
-// 		/// An example dispatchable that may throw a custom error.
-// 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-// 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-// 			let _who = ensure_signed(origin)?;
-
-// 			// Read a value from storage.
-// 			match <Something<T>>::get() {
-// 				// Return an error if the value has not been set.
-// 				None => return Err(Error::<T>::NoneValue.into()),
-// 				Some(old) => {
-// 					// Increment the value read from storage; will error in the event of overflow.
-// 					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-// 					// Update the value in storage with the incremented result.
-// 					<Something<T>>::put(new);
-// 					Ok(())
-// 				},
-// 			}
-// 		}
-// 	}
-// }
